e90e1633cdffd5815d579f42d4fe09fba7e53163
Bernardo <bersimoes@gmail.com>
2014-10-26 12:09:44 +0000

    added presentation slides; fixed course and juri names

---
+++ presentation/make.sh
@@ -0,0 +1,9 @@
+# build presentation slides 
+pdflatex wraperNoNotes.tex
+
+# build presentations notes
+pdflatex wraperNotes.tex
+
+# open notes and slides
+open wraperNotes.pdf
+open wraperNoNotes.pdf
